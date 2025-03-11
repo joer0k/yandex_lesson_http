b4cb9e8fd09292551eb7e2771934e92c19670221
joer0k <ryabokon.niki08@gmail.com>
2025-03-11 17:33:00 +0300

    угадай-ка город

--- new.py
+++ new.py
@@ -1,7 +1,41 @@
+from random import choice
+
+import pygame
+
 from support import *
 
-toponym_to_find = " ".join(sys.argv[1:])
-toponym = get_geocode_json(toponym_to_find)
-toponym_coodrinates = toponym["Point"]["pos"]
+cities = [elem.strip() for elem in open('cities.txt', encoding='utf8').readlines()]
 
-print(get_district(toponym_coodrinates)['name'])
+
+def get_map(city):
+    toponym = get_geocode_json(city)
+    toponym_coodrinates = toponym["Point"]["pos"]
+    spn = ','.join(map(lambda x: str(float(x) / 10),
+                       get_spn(toponym['boundedBy']['Envelope']['lowerCorner'],
+                               toponym['boundedBy']['Envelope']['upperCorner']).split(',')))
+    response = get_staticmap(toponym_coodrinates, spn)
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+
+
+def show_map(map_file):
+    pygame.init()
+    screen = pygame.display.set_mode((600, 450))
+    screen.blit(pygame.image.load(map_file), (0, 0))
+    running = True
+    pygame.display.flip()
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                get_map(choice(cities))
+                screen.blit(pygame.image.load(map_file), (0, 0))
+                pygame.display.flip()
+
+
+if __name__ == '__main__':
+    get_map(choice(cities))
+    show_map('map.png')
